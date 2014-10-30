5c456e5c5e3ececede3cda5074ed4a133a518488
pmohan6 <paritosh.mohan@gmail.com>
2014-10-29 22:58:19 -0700

    Adding a WorkflowReplayer for Ruby Flow and necessary tests

---
+++ aws-flow/lib/aws/replayer.rb
@@ -0,0 +1,158 @@
+module AWS
+  module Flow
+    module Replayer
+
+      # This class is used by the Replayer to fetch the DecisionTask which will
+      # be used by the DecisionTaskHandler. This is an 'abstract' class. We need
+      # to extend it and implement get_history_page and get_execution_info
+      # methods to use it.
+      class DecisionTaskProvider
+
+        # This method fetches the workflow history and wraps all the history events,
+        # workflow type, workflow execution inside a decision task for the
+        # decider to work on
+        def get_decision_task(replay_upto = nil)
+          # Get workflow execution info so that we can populate the workflowType
+          # and execution fields of the DecisionTask.
+          execution_info = get_execution_info
+          events = get_history
+
+          # Truncate history if replay_upto variable is set so that we only
+          # replay the history till the specified event
+          events = truncate_history(events, replay_upto)
+          return nil if events.nil?
+
+          # Generate the hash to instantiate a DecisionTask. We can set
+          # taskToken and nextPageToken to nil since we don't need the values
+          # in the replayer
+          data = {
+            'taskToken' => nil,
+            'workflowExecution' => execution_info["execution"],
+            'workflowType' => execution_info["workflowType"],
+            'events' => events,
+            'nextPageToken' => nil
+          }
+          AWS::SimpleWorkflow::DecisionTask.new(nil, nil, data)
+        end
+
+        # This method truncates the workflow history to the event_id specified
+        # by the replay_upto variable
+        def truncate_history(events, replay_upto = nil)
+          return nil if events.nil? || events.empty?
+
+          # Just return the original array of events if replay_upto is not set
+          # or if the number of events is less than replay_upto
+          return events if replay_upto.nil? || events.last['eventId'] <= replay_upto
+
+          # Select the events whose eventId is lesser than replay_upto
+          truncated = events.select { |event| event['eventId'] <= replay_upto }
+          return nil if truncated.empty?
+          truncated
+        end
+
+        # This method is used to fetch the actual history. Implementing classes
+        # must override this method.
+        def get_history(page_token = nil); end
+
+        # This method is used to fetch the WorkflowExecutionInfo to fill in the
+        # DecisionTask details. Implementing classes must override this method
+        def get_execution_info; end
+
+      end
+
+      # This DecisionTaskProvider loads the decision task directly from the
+      # SimpleWorkflowService
+      class ServiceDecisionTaskProvider < DecisionTaskProvider
+        attr_reader :domain, :execution, :swf
+
+        def initialize(options = {})
+          raise ArgumentError.new("options hash must contain :domain") if options[:domain].nil?
+          raise ArgumentError.new("options hash must contain :execution") if options[:execution].nil?
+          @execution = options[:execution]
+          @domain = options[:domain]
+          @swf = AWS::SimpleWorkflow.new.client
+        end
+
+        def get_history
+          events = []
+          # Get the first page of the workflow history
+          page = get_history_page
+          page["events"].each { |x| events << x }
+
+          # Get the remaining pages of the workflow history
+          until page["nextPageToken"].nil?
+            page = get_history_page(page["nextPageToken"])
+            page["events"].each { |x| events << x }
+          end
+          events
+        end
+
+        # This method calls the service to fetch a page of workflow history
+        def get_history_page(page_token = nil)
+          # generate the request options for the service call. Optionally merge
+          # next_page_token to the hash if the page_token value is not nil.
+          request_opts = {
+            domain: @domain,
+            execution: @execution,
+          }.merge(page_token ? { next_page_token: page_token } : {})
+
+          @swf.get_workflow_execution_history(request_opts)
+        end
+
+        # This method calls the service to get the workflow execution
+        # information
+        def get_execution_info
+          execution = @swf.describe_workflow_execution(
+            domain: @domain,
+            execution: @execution
+          )
+          execution["executionInfo"]
+        end
+
+      end
+
+      # WorkflowReplayer is an AWS Flow Framework utility that is used to
+      # 'replay' a workflow history in the decider against the workflow
+      # implementation. It is a useful debugging tool.
+      #
+      # Usage -
+      #
+      # # Create an instance of the replayer with the required options -
+      #
+      # replayer = AWS::Flow::Replayer::WorkflowReplayer.new(
+      #   domain: '<domain_name>',
+      #   execution: {
+      #     workflow_id: "<workflow_id",
+      #     run_id: "<run_id>"
+      #   },
+      #   workflow_class: WorkflowClass
+      # )
+      #
+      # # Call the replay method (optionally) with the replay_upto event_id number -
+      #
+      # decision = replayer.replay(20)
+      #
+      class WorkflowReplayer
+        attr_reader :task_handler, :task_provider
+
+        def initialize(options)
+          raise ArgumentError.new("You must pass in an options hash") if options.nil?
+          raise ArgumentError.new("options hash must contain :workflow_class") if options[:workflow_class].nil?
+
+          # Create the service decision task helper to fetch and truncate the
+          # history
+          @task_provider = ServiceDecisionTaskProvider.new(options)
+          @task_handler = DecisionTaskHandler.from_workflow_class(options[:workflow_class])
+        end
+
+        # This method performs the actual replay.
+        def replay(replay_upto = nil)
+          task = @task_provider.get_decision_task(replay_upto)
+          @task_handler.handle_decision_task(task) unless task.nil?
+        end
+
+      end
+
+    end
+  end
+end
---
+++ aws-flow/spec/aws/replayer/integration/replayer_spec.rb
@@ -0,0 +1,91 @@
+require "spec_helper"
+include Test::Integ
+
+describe Replayer do
+  before(:all) do
+
+    @swf, @domain = setup_swf("ReplayerTest")
+
+    class ReplayerTestActivity
+      extend Activities
+      activity :activity_a do
+        {
+          version: "1.0"
+        }
+      end
+      def activity_a; end
+    end
+
+    class ReplayerTestWorkflow
+      extend Workflows
+      workflow :small, :large do
+        {
+          version: "1.0",
+          default_execution_start_to_close_timeout: 600
+        }
+      end
+      activity_client(:client) { { from_class: "ReplayerTestActivity" } }
+      def small
+        client.activity_a
+      end
+      def large
+        25.times { client.send_async(:activity_a) }
+        create_timer(1)
+      end
+    end
+
+    class ReplayerIntegTest
+      class << self
+        def run(domain, method)
+          workflow_worker = AWS::Flow::WorkflowWorker.new(domain.client, domain, "replayer_wf_tasklist", ReplayerTestWorkflow)
+          activity_worker = AWS::Flow::ActivityWorker.new(domain.client, domain, "replayer_act_tasklist", ReplayerTestActivity)
+          workflow_worker.register
+          activity_worker.register
+
+          executor = AWS::Flow::ForkingExecutor.new(max_workers: 5)
+          executor.execute { activity_worker.start }
+          executor.execute { workflow_worker.start }
+
+          client = AWS::Flow::workflow_client(domain.client, domain) { { from_class: "ReplayerTestWorkflow" } }
+          execution = client.send(method)
+          wait_for_execution(execution)
+          executor.shutdown(1)
+          execution
+        end
+      end
+    end
+  end
+
+  it "tests a small workflow history as a sanity test" do
+    execution = ReplayerIntegTest.run(@domain, :small)
+    replayer = WorkflowReplayer.new(
+      domain: @domain.name,
+      execution: {
+        workflow_id: execution.workflow_id,
+        run_id: execution.run_id
+      },
+      workflow_class: ReplayerTestWorkflow
+    )
+
+    replayer.replay(32)[:decisions].first[:decision_type].should == "CompleteWorkflowExecution"
+  end
+
+  it "tests a large workflow history to ensure paged histories are loaded correctly" do
+    execution = ReplayerIntegTest.run(@domain, :large)
+    replayer = WorkflowReplayer.new(
+      domain: @domain.name,
+      execution: {
+        workflow_id: execution.workflow_id,
+        run_id: execution.run_id
+      },
+      workflow_class: ReplayerTestWorkflow
+    )
+
+    decisions = replayer.replay(3)
+    decisions = decisions[:decisions].map{ |x| x[:decision_type] }
+    decisions.select { |x| x == "ScheduleActivityTask" }.size.should == 25
+    decisions.select { |x| x == "StartTimer" }.size.should == 1
+
+  end
+
+end
